a874b098445ec060bed3f1befad1a434cf13b860
Mathieu Poignet <mathieu.poignet@gmail.com>
2021-02-08 17:13:00 +0100

    initial commit. Only basic features of adding and removing markers and routes are implemented

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import MappingClient from './mappingClient.js'
+import './style.css';
+
+const SEARCH_INPUT_ID = "search-input";
+const SEARCH_BTN_ID = "search-btn";
+const ROUTE_BTN_ID = "route-btn";
+const CLEAR_BTN_ID = "clear-btn";
+
+window.onload = function () {
+  const mappingClient = new MappingClient("map");
+
+  document.getElementById(SEARCH_INPUT_ID).addEventListener("keyup", (e) => {
+    let textToSearch = document.getElementById(SEARCH_INPUT_ID).value;
+    let dataList = document.getElementById("suggestions");
+    dataList.innerHTML = "";
+
+    // Test for Enter key
+    if (e.keyCode == 13) {
+      mappingClient.plotPointFromText(textToSearch);
+    } else if (textToSearch.length > 3) {
+      mappingClient.getAutocomplete(textToSearch, (suggestions) => {
+        console.debug("Results of autocomplete: ");
+        console.debug(suggestions);
+        suggestions.forEach((suggestion) => {
+          const option = document.createElement("option");
+          option.value = suggestion;
+          dataList.appendChild(option);
+        });
+      });
+    }
+  });
+
+  document.getElementById(CLEAR_BTN_ID).addEventListener("click", (e) => {
+    mappingClient.clearMap();
+  });
+
+  document.getElementById(SEARCH_BTN_ID).addEventListener("click", (e) => {
+    mappingClient.plotPointFromText(
+      document.getElementById(SEARCH_INPUT_ID).value
+    );
+  });
+
+  document.getElementById(ROUTE_BTN_ID).addEventListener("click", (e) => {
+    mappingClient.plotPointFromText(
+      document.getElementById(SEARCH_INPUT_ID).value
+    );
+    addDestinationToPanel();
+  });
+
+  function addDestinationToPanel() {
+    const searchLine = document.createElement("div");
+    searchLine.className += "search-line";
+    const searchInput = document.createElement("input");
+    searchInput.setAttribute("type", "text");
+    searchInput.className = "panel-input";
+    searchLine.appendChild(searchInput);
+    const dataList = document.createElement("datalist");
+    searchLine.appendChild(dataList);
+    const button = document.createElement("button");
+    button.id = "destination-btn";
+    button.className = "panel-btn";
+    const icon = document.createElement("i");
+    icon.className += "fas fa-directions";
+    button.appendChild(icon);
+    button.addEventListener("click", function (e) {
+      mappingClient.plotRouteFromText(
+        document.getElementById(SEARCH_INPUT_ID).value,
+        searchInput.value
+      );
+    });
+    searchLine.appendChild(button);
+    document.getElementById("panel").appendChild(searchLine);
+  }
+};
---
+++ src/mappingClient.js
@@ -0,0 +1,221 @@
+import Openrouteservice from "openrouteservice-js";
+import L from "leaflet";
+import "leaflet-contextmenu";
+
+const API_TOKEN = "5b3ce3597851110001cf6248bd9653bdfbb34639abfd8b798225be02";
+const LayerType = {
+  MARKER: "marker",
+  ROUTE: "route",
+};
+
+class MappingClient {
+  // mapIdd: the id of the DOM element to load the map
+  constructor(mapId) {
+    this.orsDirections = new Openrouteservice.Directions({
+      api_key: API_TOKEN,
+    });
+    this.Geocode = new Openrouteservice.Geocode({
+      api_key: API_TOKEN,
+    });
+    this.orsUtil = new Openrouteservice.Util();
+    const self = this;
+
+    //Bindings
+    this.plotPointFromEvent = this.plotPointFromEvent.bind(this);
+    this.plotRouteFromEvent = this.plotRouteFromEvent.bind(this);
+
+    // Keep track of the current stops for the route
+    this.stops = [];
+
+    // Add an autocomplete function to the SDK
+    this.Geocode.autocomplete = function (reqArgs) {
+      // Get custom header and remove from args
+      this.customHeaders = [];
+      if (reqArgs.customHeaders) {
+        this.customHeaders = reqArgs.customHeaders;
+        delete reqArgs.customHeaders;
+      }
+      self.orsUtil.setRequestDefaults(this.args, reqArgs);
+      reqArgs.service = "geocode/autocomplete";
+      self.orsUtil.copyProperties(reqArgs, this.args);
+      return this.geocodePromise();
+    };
+
+    //Initialize Leaflet
+    this.map = L.map(mapId, {
+      contextmenu: true,
+      contextmenuWidth: 140,
+      contextmenuItems: [
+        {
+          text: "Add marker",
+          callback: this.plotPointFromEvent,
+        },
+        {
+          text: "Add route",
+          callback: this.plotRouteFromEvent,
+        },
+      ],
+    }).setView({ lon: 0, lat: 0 }, 2);
+
+    // add the OpenStreetMap tiles
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        "&copy; <a href='https://openstreetmap.org/copyright'>OpenStreetMap contributors</a>",
+    }).addTo(this.map);
+
+    // show the scale bar on the lower left corner
+    L.control.scale().addTo(this.map);
+  }
+
+  addEventListener(...args) {
+    this.map.addEventListener(...args);
+  }
+
+  deleteLayer(e) {
+    console.debug("deleting layer");
+    console.debug(e);
+    this.remove();
+  }
+
+  addLayer(type, object) {
+    let layerConstructor;
+    switch (type) {
+      case LayerType.MARKER: layerConstructor = L.marker; break;
+      case LayerType.ROUTE: layerConstructor = L.geoJson; break;
+    }
+    const layer = layerConstructor(object, {
+      contextmenu: true,
+      contextmenuWidth: 140,
+      contextmenuItems: [
+        {
+          text: "Delete",
+          callback: this.deleteLayer,
+        },
+      ],
+    });
+    layer.options.contextmenuItems[0].context = layer;
+    layer.addTo(this.map);
+  }
+
+  plotPointFromCoordinates(coordinates) {
+    console.debug(coordinates);
+    this.addLayer(LayerType.MARKER, {
+      lon: coordinates.lng,
+      lat: coordinates.lat,
+    });
+  }
+
+  plotPointFromEvent(e) {
+    console.debug("adding marker from contextmenu");
+    this.plotPointFromCoordinates(e.latlng);
+  }
+
+  plotRouteFromEvent(e) {
+    console.debug("adding route from contextmenu");
+    this.plotPointFromCoordinates(e.latlng);
+    this.stops.push(e.latlng);
+    if (this.stops.length == 2) {
+      this.plotRouteFromCoordinates(this.stops[0], this.stops[1]);
+    }
+  }
+
+  plotPointFromText(text) {
+    console.debug("Searching for " + text);
+    let self = this;
+    this.Geocode.geocode({
+      text: text,
+      size: 1,
+    })
+      .then(function (geojson) {
+        console.debug(
+          "Plotting point with coordinates " +
+            geojson.features[0].geometry.coordinates
+        );
+        const coords = geojson.features[0].geometry.coordinates;
+        self.plotPointFromCoordinates({ lng: coords[0], lat: coords[1] });
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+  plotRouteFromCoordinates(origin, destination) {
+    console.debug("plotting route between " + origin + " and " + destination);
+    let self = this;
+    this.orsDirections
+      .calculate({
+        coordinates: [
+          [origin.lng, origin.lat],
+          [destination.lng, destination.lat],
+        ],
+        profile: "driving-car",
+        instructions: "false",
+        format: "geojson",
+      })
+      .then(function (geojson) {
+        console.debug("plotting route :");
+        console.debug(geojson);
+        self.addLayer(LayerType.ROUTE, geojson);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+  plotRouteFromText(pointA, pointB) {
+    console.debug("searching for route between " + pointA + " and " + pointB);
+    let self = this;
+    Promise.allSettled([
+      this.Geocode.geocode({
+        text: pointA,
+        size: 1,
+      }),
+      this.Geocode.geocode({
+        text: pointB,
+        size: 1,
+      }),
+    ])
+      .then(function (response) {
+        const origin = self.getLatLngFromGeojson(response[0]);
+        const destination = self.getLatLngFromGeojson(response[1]);
+        self.plotPointFromCoordinates(origin);
+        self.plotPointFromCoordinates(destination);
+        self.plotRouteFromCoordinates(origin, destination);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+  getAutocomplete(text, callback) {
+    console.debug("searching autocomplete for " + text);
+    this.Geocode.autocomplete({
+      text: text,
+    })
+      .then(function (result) {
+        callback(result.features.map((e) => e.properties.label));
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+  clearMap() {
+    this.map.eachLayer((layer) => {
+      if (!(layer instanceof L.TileLayer)) {
+        console.debug("removing layer");
+        layer.remove();
+      }
+    });
+  }
+
+  getLatLngFromGeojson(geojson) {
+    return {
+      lng: geojson.value.features[0].geometry.coordinates[0],
+      lat: geojson.value.features[0].geometry.coordinates[1],
+    };
+  }
+}
+
+export default MappingClient
