59c3db6bde00f33959320d285359f85db6b636f7
Mathieu Poignet <mathieu.poignet@gmail.com>
2021-02-10 10:35:06 +0100

    changing linting style to standard

--- src/index.js
+++ src/index.js
@@ -1,76 +1,76 @@
 import MappingClient from './mappingClient.js'
-import "normalize.css"
-import '@fortawesome/fontawesome-free/js/all.js';
-import './style.css';
+import 'normalize.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import './style.css'
 
-const SEARCH_INPUT_ID = "search-input";
-const SEARCH_BTN_ID = "search-btn";
-const ROUTE_BTN_ID = "route-btn";
-const CLEAR_BTN_ID = "clear-btn";
+const SEARCH_INPUT_ID = 'search-input'
+const SEARCH_BTN_ID = 'search-btn'
+const ROUTE_BTN_ID = 'route-btn'
+const CLEAR_BTN_ID = 'clear-btn'
 
 window.onload = function () {
-  const mappingClient = new MappingClient("map");
+  const mappingClient = new MappingClient('map')
 
-  document.getElementById(SEARCH_INPUT_ID).addEventListener("keyup", (e) => {
-    let textToSearch = document.getElementById(SEARCH_INPUT_ID).value;
-    let dataList = document.getElementById("suggestions");
-    dataList.innerHTML = "";
+  document.getElementById(SEARCH_INPUT_ID).addEventListener('keyup', (e) => {
+    const textToSearch = document.getElementById(SEARCH_INPUT_ID).value
+    const dataList = document.getElementById('suggestions')
+    dataList.innerHTML = ''
 
     // Test for Enter key
-    if (e.keyCode == 13) {
-      mappingClient.plotPointFromText(textToSearch);
+    if (e.keyCode === 13) {
+      mappingClient.plotPointFromText(textToSearch)
     } else if (textToSearch.length > 3) {
       mappingClient.getAutocomplete(textToSearch, (suggestions) => {
-        console.debug("Results of autocomplete: ");
-        console.debug(suggestions);
+        console.debug('Results of autocomplete: ')
+        console.debug(suggestions)
         suggestions.forEach((suggestion) => {
-          const option = document.createElement("option");
-          option.value = suggestion;
-          dataList.appendChild(option);
-        });
-      });
+          const option = document.createElement('option')
+          option.value = suggestion
+          dataList.appendChild(option)
+        })
+      })
     }
-  });
+  })
 
-  document.getElementById(CLEAR_BTN_ID).addEventListener("click", (e) => {
-    mappingClient.clearMap();
-  });
+  document.getElementById(CLEAR_BTN_ID).addEventListener('click', (e) => {
+    mappingClient.clearMap()
+  })
 
-  document.getElementById(SEARCH_BTN_ID).addEventListener("click", (e) => {
+  document.getElementById(SEARCH_BTN_ID).addEventListener('click', (e) => {
     mappingClient.plotPointFromText(
       document.getElementById(SEARCH_INPUT_ID).value
-    );
-  });
+    )
+  })
 
-  document.getElementById(ROUTE_BTN_ID).addEventListener("click", (e) => {
+  document.getElementById(ROUTE_BTN_ID).addEventListener('click', (e) => {
     mappingClient.plotPointFromText(
       document.getElementById(SEARCH_INPUT_ID).value
-    );
-    addDestinationToPanel();
-  });
+    )
+    addDestinationToPanel()
+  })
 
-  function addDestinationToPanel() {
-    const searchLine = document.createElement("div");
-    searchLine.className += "search-line";
-    const searchInput = document.createElement("input");
-    searchInput.setAttribute("type", "text");
-    searchInput.className = "panel-input";
-    searchLine.appendChild(searchInput);
-    const dataList = document.createElement("datalist");
-    searchLine.appendChild(dataList);
-    const button = document.createElement("button");
-    button.id = "destination-btn";
-    button.className = "panel-btn";
-    const icon = document.createElement("i");
-    icon.className += "fas fa-directions";
-    button.appendChild(icon);
-    button.addEventListener("click", function (e) {
+  function addDestinationToPanel () {
+    const searchLine = document.createElement('div')
+    searchLine.className += 'search-line'
+    const searchInput = document.createElement('input')
+    searchInput.setAttribute('type', 'text')
+    searchInput.className = 'panel-input'
+    searchLine.appendChild(searchInput)
+    const dataList = document.createElement('datalist')
+    searchLine.appendChild(dataList)
+    const button = document.createElement('button')
+    button.id = 'destination-btn'
+    button.className = 'panel-btn'
+    const icon = document.createElement('i')
+    icon.className += 'fas fa-directions'
+    button.appendChild(icon)
+    button.addEventListener('click', function (e) {
       mappingClient.plotRouteFromText(
         document.getElementById(SEARCH_INPUT_ID).value,
         searchInput.value
-      );
-    });
-    searchLine.appendChild(button);
-    document.getElementById("panel").appendChild(searchLine);
+      )
+    })
+    searchLine.appendChild(button)
+    document.getElementById('panel').appendChild(searchLine)
   }
-};
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -14,14 +14,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
               ['@babel/preset-env',
-                { 
-                targets: "defaults",
-                "useBuiltIns": "usage",
-                "corejs": "3.8.3"
+                {
+                  targets: 'defaults',
+                  useBuiltIns: 'usage',
+                  corejs: '3.8.3'
                 }
               ]
             ]
@@ -30,20 +30,20 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
 
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
 
-        type: 'asset/resource',
+        type: 'asset/resource'
 
-      },
-    ],
+      }
+    ]
   },
   resolve: {
     extensions: [
       '.js'
     ]
   }
-};
+}
